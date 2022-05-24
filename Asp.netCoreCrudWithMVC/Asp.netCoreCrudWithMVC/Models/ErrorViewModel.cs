e5f363c894a2171b4b6b2c5651d6e43ae27aef60
obaydullah <obaydullah599@gmail.com>
2022-05-24 16:03:04 +0600

    DotCoreCrud

---
+++ Asp.netCoreCrudWithMVC/Asp.netCoreCrudWithMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.netCoreCrudWithMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Asp.netCoreCrudWithMVC/Asp.netCoreCrudWithMVC/Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿namespace Asp.netCoreCrudWithMVC.Models
+{
+    public static class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+        public static void Create(Employee employee)
+        {
+            allEmpoyees.Add(employee);
+        }
+        public static void Delete(Employee employee)
+        {
+            allEmpoyees.Remove(employee);
+        }
+    }
+}
